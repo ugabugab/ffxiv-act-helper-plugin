ae246f7e75e1cfe0aa734fe4a32a24669896a660
ugabugab <ugabugab@gmail.com>
2021-01-24 02:40:26 +0900

    Add project

---
+++ FFXIV_ACT_Helper_Plugin/PluginMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace FFXIV_ACT_Helper_Plugin
+{
+    partial class PluginMain
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(15, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(194, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "End combat when restart content";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // PluginMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkBox1);
+            this.Name = "PluginMain";
+            this.Size = new System.Drawing.Size(631, 361);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ FFXIV_ACT_Helper_Plugin/PluginMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Diagnostics;
+using Advanced_Combat_Tracker;
+
+namespace FFXIV_ACT_Helper_Plugin
+{
+    public partial class PluginMain : UserControl, IActPluginV1
+    {
+        Label lblStatus;    // The status label that appears in ACT's Plugin tab
+        string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\FFXIV_ACT_Helper_Plugin.config.xml");
+        SettingsSerializer xmlSettings;
+
+        public PluginMain()
+        {
+            InitializeComponent();
+        }
+
+        public void DeInitPlugin()
+        {
+            ActGlobals.oFormActMain.BeforeLogLineRead -= oFormActMain_BeforeLogLineRead;
+
+            SaveSettings();
+            lblStatus.Text = "Plugin Exited";
+        }
+
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            lblStatus = pluginStatusText;   // Hand the status label's reference to our local var
+            pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
+            this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
+            xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
+            LoadSettings();
+
+            ActGlobals.oFormActMain.BeforeLogLineRead += new LogLineEventDelegate(oFormActMain_BeforeLogLineRead);
+
+            lblStatus.Text = "Plugin Started";
+        }
+
+        void oFormActMain_BeforeLogLineRead(bool isImport, LogLineEventArgs logInfo)
+        {
+            //Debug.WriteLine(logInfo.originalLogLine);
+
+            try
+            {
+                string[] logComponents = logInfo.originalLogLine.Split('|');
+
+                // End combat when restarted content
+                if (checkBox1.Checked)
+                {
+                    // eg. 33|2021-01-23T16:34:42.9370000+09:00|8003757B|40000006|14E3|14|00|00|4f1194ca3def5c41059c5e69ffc7689a
+                    if (logComponents[0] == "33" && logComponents[3] == "40000006")
+                    {
+                        ActGlobals.oFormActMain.EndCombat(false);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        void LoadSettings()
+        {
+            // Add any controls you want to save the state of
+            xmlSettings.AddControlSetting(checkBox1.Name, checkBox1);
+
+            if (File.Exists(settingsFile))
+            {
+                FileStream fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                XmlTextReader xReader = new XmlTextReader(fs);
+
+                try
+                {
+                    while (xReader.Read())
+                    {
+                        if (xReader.NodeType == XmlNodeType.Element)
+                        {
+                            if (xReader.LocalName == "SettingsSerializer")
+                            {
+                                xmlSettings.ImportFromXml(xReader);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "Error loading settings: " + ex.Message;
+                }
+                xReader.Close();
+            }
+        }
+
+        void SaveSettings()
+        {
+            FileStream fs = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            XmlTextWriter xWriter = new XmlTextWriter(fs, Encoding.UTF8);
+            xWriter.Formatting = Formatting.Indented;
+            xWriter.Indentation = 1;
+            xWriter.IndentChar = '\t';
+            xWriter.WriteStartDocument(true);
+            xWriter.WriteStartElement("Config");    // <Config>
+            xWriter.WriteStartElement("SettingsSerializer");    // <Config><SettingsSerializer>
+            xmlSettings.ExportToXml(xWriter);   // Fill the SettingsSerializer XML
+            xWriter.WriteEndElement();  // </SettingsSerializer>
+            xWriter.WriteEndElement();  // </Config>
+            xWriter.WriteEndDocument(); // Tie up loose ends (shouldn't be any)
+            xWriter.Flush();    // Flush the file buffer to disk
+            xWriter.Close();
+        }
+
+    }
+}
